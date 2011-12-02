2fc7256d7b0167618571e8a01987568c95488961
James Hall <jmh045000@utdallas.edu>
2011-12-01 20:49:48 -0600

    missed comments

--- usage.h
+++ usage.h
@@ -1,6 +1,7 @@
 #ifndef __USAGE_H__
 #define __USAGE_H__
 
+//Some simple functions to print usage messages
 void router_usage(const char * errmsg=NULL, bool exit=true, int rc=1);
 void PCE_usage(const char * errmsg=NULL, bool exit=true, int rc=1);
 
